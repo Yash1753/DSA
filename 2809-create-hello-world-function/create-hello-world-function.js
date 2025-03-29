f91d02aefa670d2fc5c62942b68f49d35e8689b9
Yash Rohilla <110462053+Yash1753@users.noreply.github.com>
2025-03-29 22:03:21 +0530

    Time: 45 ms (47.72%) | Memory: 53.6 MB (24.35%) - LeetSync

---
+++ 2809-create-hello-world-function/create-hello-world-function.js
@@ -0,0 +1,15 @@
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    
+    return function(...args) {
+       var a = "Hello World";
+       return a;
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
