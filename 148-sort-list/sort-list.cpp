4f386726d3ded2f4ef33fc71abd0a80a7b3fd9d4
Yash Rohilla <110462053+Yash1753@users.noreply.github.com>
2025-06-02 14:37:11 +0530

    Time: 43 ms (48.59%) | Memory: 75.7 MB (21.97%) - LeetSync

--- 148-sort-list/sort-list.cpp
+++ 148-sort-list/sort-list.cpp
@@ -10,24 +10,56 @@
  */
 class Solution {
 public:
+    ListNode* middleLL(ListNode * head){
+        ListNode * slow = head;
+        ListNode * fast= head;
+        ListNode * s1= NULL;
+
+        while(fast && fast->next){
+            s1 = slow;
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        return s1; //return middle 1 in even to find correct point to break;
+    }
+
+
+    ListNode* mergeLL(ListNode * left , ListNode * right){
+        ListNode* t1 = left;
+        ListNode* t2 = right;
+        ListNode* dummy = new ListNode(-1);
+        ListNode * temp = dummy;
+
+        while(t1 &&  t2){
+            if(t1->val< t2->val){
+                temp->next = t1;
+                temp = t1;
+                t1 = t1->next;
+            }else{
+                temp->next = t2;
+                temp = t2;
+                t2 = t2->next;
+            }
+        }
+
+        if(t1) temp->next = t1;
+        else temp->next = t2;
+
+        return dummy->next;
+
+
+    }
+
     ListNode* sortList(ListNode* head) {
+        ListNode* middle = middleLL(head);
         if(!head || !head->next) return head;
-        vector<int>s;
-        ListNode * temp = head;
-        while(temp){
-            s.push_back(temp->val);
-            temp = temp->next;
-        }
-        temp = head;
-        
-        sort(s.begin(),s.end());
-
-        for(auto it: s){
-            temp->val = it;
-            temp = temp->next;
-        }
-
-        return head;
+        ListNode * left = head;
+        ListNode * right = middle->next;
+        middle->next = NULL;
+        left = sortList(left);
+        right = sortList(right);
+        return mergeLL(left,right);
 
     }
 };
