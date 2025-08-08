ed9fdb857b9a73d09900be63cbf1183ad43803e2
Yash Rohilla <110462053+Yash1753@users.noreply.github.com>
2025-08-08 15:44:37 +0530

    Time: 6 ms (7.68%) | Memory: 18.8 MB (7.75%) - LeetSync

---
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool isBipartite(vector<vector<int>>& graph) {
+        unordered_map<int,vector<int>> adj;
+        int n = graph.size();
+        vector<int>color(n,-1);
+
+        for(int v  = 0 ; v < n ; v++){
+            adj[v] = graph[v];
+        }
+
+        for(int i = 0 ; i <n ; i++){
+            if(color[i] == -1){
+                if(!DFS(adj,i,color,1)) return false;
+            }
+            
+        }
+
+        return true;
+    }
+
+private:
+    bool DFS( unordered_map<int,vector<int>> &adj,int u,  vector<int>&color, int CC ){
+        color[u] = CC;
+
+        for(int &v : adj[u]){
+            if(color[v] == color[u]) return false;
+
+            if(color[v] == -1){
+                if(!DFS(adj,v,color,1-CC)) return false;
+            }
+        }
+
+        return true;
+    }
+};
