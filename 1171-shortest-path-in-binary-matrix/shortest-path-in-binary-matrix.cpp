7deb5ce98d431d1199bb6a88e55e156e27ae7df8
Yash Rohilla <110462053+Yash1753@users.noreply.github.com>
2025-08-12 20:51:11 +0530

    Time: 254 ms (7.05%) | Memory: 82.8 MB (5.75%) - LeetSync

---
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    //traverse diff directions
+    vector<vector<int>>directions{{1,1}, {1,0}, {0,1}, {-1,0}, {0,-1}, {-1,1},{1,-1}, {-1,-1}};
+
+    int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+
+        if(m==0 || n==0 || grid[0][0] != 0) return -1;
+        int level = 0;
+
+        auto isSafe = [&](int x, int y){
+            if(x>=0 && x<m && y>=0 && y<n) return true;
+            return false;
+        };
+
+        queue<pair<int,int>>q;
+        q.push({0,0}); //index
+        
+        while(!q.empty()){
+            int N = q.size();
+            while(N--){
+                auto top = q.front();q.pop();
+                int x = top.first;
+                int y = top.second;
+
+                if(x == m-1 && y==n-1) return level+1;
+
+                for(auto dir : directions){
+                    int x_ = x + dir[0];
+                    int y_ = y + dir[1];
+
+                    if(isSafe(x_,y_) && grid[x_][y_] == 0){
+                        grid[x_][y_] = 1;
+                        q.push({x_,y_});
+                    }
+                }
+                
+            }
+            level++;
+        }
+
+        return -1;
+    }
+};
