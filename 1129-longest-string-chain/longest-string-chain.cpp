e997bde11fdf311d1cb2d36bbcc51786d65116b4
Yash Rohilla <110462053+Yash1753@users.noreply.github.com>
2025-08-28 16:50:43 +0530

    Time: 267 ms (28.81%) | Memory: 389 MB (25.71%) - LeetSync

--- 1129-longest-string-chain/longest-string-chain.cpp
+++ 1129-longest-string-chain/longest-string-chain.cpp
@@ -1,40 +1,55 @@
 class Solution {
 public:
-    bool isPossible(string &s1, string &s2){
-        if(s1.size()!=s2.size()+1) return false;
-        int i=0,j=0;
-        while(i<s1.size()){
-            if(j<s2.size() && s1[i]==s2[j]){
+    int n;
+    vector<vector<int>>dp;
+    static bool lambda(string &s1, string &s2){
+        return s1.length() < s2.length();
+    }
+        bool isPred(string& prev, string& curr) {
+        int M = prev.length();
+        int N = curr.length();
+        
+        if(M >= N || N-M != 1)
+            return false;
+        
+        int i = 0, j = 0;
+        while(i < M && j < N) {
+            if(prev[i] == curr[j]) {
                 i++;
-                j++;
             }
-            else{
-                i++;
-            }
+            j++;
         }
-        return j==s2.size();
+        return i==M;
+    }
+    
+
+    int LIS(vector<string>& words, int curr , int prev){
+        if(curr == n) return 0;
+
+        if(prev !=-1 && dp[curr][prev] != -1) return dp[curr][prev];
+        int taken = 0;
+        if(prev == -1 || isPred(words[prev], words[curr]))
+            taken = 1 + LIS(words, curr+1, curr);
+        
+        int not_taken = LIS(words, curr+1, prev);
+
+        if(prev!=-1){
+            dp[curr][prev] = max(taken,not_taken);
+        }
+
+        return max(taken,not_taken);
     }
 
-
-   static bool comp (string &s1, string &s2) {
-        return s1.size() < s2.size();
-    }
 
     int longestStrChain(vector<string>& words) {
-        int n = words.size();
-         sort(words.begin(), words.end(), comp);
-        vector<int> dp (n,1);
-        int maxi = 1;
-        for(int i = 1 ; i < n ;i++){
-            for(int j = 0 ; j < i ; j++){
-                if(isPossible(words[i],words[j]) && dp[i] < dp[j]+1  ){
-                    dp[i] = dp[j]+1;
-                }
-            }
+        //words array h
+        //chain chaiye maxx length to make words good such that predecoors ek change and seqyuenmce sAME    
+         n = words.size();
+         dp.resize(1001,vector<int>(1001,-1));
+        sort(words.begin(), words.end(), lambda);
+       
+        return LIS(words, 0, -1); //words, ind, prev
 
-            maxi = max(maxi, dp[i]);
-        }
 
-        return maxi;
     }
 };
