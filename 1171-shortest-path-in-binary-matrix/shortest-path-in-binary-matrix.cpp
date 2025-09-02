f898bf9eee3ed09b41396ccbdc728d3b0fe2da87
Yash Rohilla <110462053+Yash1753@users.noreply.github.com>
2025-09-02 23:21:36 +0530

    Time: 7 ms (97.29%) | Memory: 23.7 MB (78.76%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -1,44 +1,36 @@
 class Solution {
 public:
-    //traverse diff directions
-    vector<vector<int>>directions{{1,1}, {1,0}, {0,1}, {-1,0}, {0,-1}, {-1,1},{1,-1}, {-1,-1}};
-
+//kind of unit wieght toh bfs work krjayega
+    int n;
+    vector<vector<int>>directions{{1,0},{0,1},{-1,0},{0,-1},{1,1},{1,-1},{-1,1},{-1,-1}};
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        int m = grid.size();
-        int n = grid[0].size();
-
-        if(m==0 || n==0 || grid[0][0] != 0) return -1;
-        int level = 0;
-
-        auto isSafe = [&](int x, int y){
-            if(x>=0 && x<m && y>=0 && y<n) return true;
-            return false;
-        };
+        n = grid.size();
+           if( n==0 || grid[0][0] != 0) return -1;
 
         queue<pair<int,int>>q;
-        q.push({0,0}); //index
-        
+        q.push({0,0});
+        int len = 1;
+
         while(!q.empty()){
             int N = q.size();
             while(N--){
-                auto top = q.front();q.pop();
-                int x = top.first;
-                int y = top.second;
+                auto front = q.front();q.pop();
+                int x = front.first;
+                int y = front.second;
 
-                if(x == m-1 && y==n-1) return level+1;
-
+                if(x==n-1&& y == n-1) return len;
                 for(auto &dir : directions){
-                    int x_ = x + dir[0];
-                    int y_ = y + dir[1];
+                    int nx = x+dir[0];
+                    int ny = y+dir[1];
 
-                    if(isSafe(x_,y_) && grid[x_][y_] == 0){
-                        grid[x_][y_] = 1;
-                        q.push({x_,y_});
+                    if(nx >=0 && nx < n && ny>=0 && ny < n && grid[nx][ny] == 0){
+                        q.push({nx,ny});
+                        grid[nx][ny] = 1;
                     }
                 }
-                
             }
-            level++;
+            len++;
+           
         }
 
         return -1;
