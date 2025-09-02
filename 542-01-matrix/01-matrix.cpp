121b630164ea1a4f79a235437984a30c57ee39e2
Yash Rohilla <110462053+Yash1753@users.noreply.github.com>
2025-09-02 14:02:37 +0530

    Time: 5 ms (96.18%) | Memory: 35 MB (66.44%) - LeetSync

--- 542-01-matrix/01-matrix.cpp
+++ 542-01-matrix/01-matrix.cpp
@@ -1,43 +1,41 @@
 class Solution {
 public:
-    vector<vector<int>>directions{{-1,0},{0,-1}, {0,1},{1,0}};
-    typedef pair<int,int>P;
+
+
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        //multisource bfs socho
-        int n = mat.size();
-        int m = mat[0].size();
-        vector<vector<int>> result(n,vector<int>(m,-1));
-        queue<P>q;
-        for(int i = 0 ; i < n ; i++){
-            for(int j = 0 ; j < m ; j++){
+        int m = mat.size();
+        int n = mat[0].size();
+        //multisources bfs
+        vector<vector<int>>directions{{0,1},{1,0},{-1,0},{0,-1}};
+        vector<vector<int>>res(m,vector<int>(n,-1));
+        queue<pair<int,int>>q; //coordinates
+
+        for(int i = 0 ; i < m ; i++ ){
+            for(int j = 0 ; j  < n ; j++){
                 if(mat[i][j] == 0){
-                    result[i][j] = 0;
+                    res[i][j] = 0;
                     q.push({i,j});
+
                 }
             }
         }
 
         while(!q.empty()){
-            int N = q.size();
-            while(N--){
-                auto top = q.front();
-                q.pop();
+            auto front = q.front();q.pop();
+            int x = front.first;
+            int y = front.second;
 
-                int i = top.first;
-                int j = top.second;
+            for(auto &dir : directions){
+                int nx = x + dir[0];
+                int ny = y + dir[1];
 
-                for(auto &dir: directions){
-                    int new_i = i + dir[0];
-                    int new_j = j + dir[1];
-
-                    if(new_i >=0 && new_i < n && new_j >=0 && new_j <m && result[new_i][new_j] == -1){
-                        q.push({new_i, new_j});
-                        result[new_i][new_j] = result[i][j] +1;
-                    }
+                if(nx >=0 && nx < m && ny >=0 && ny < n && res[nx][ny] == -1){
+                    res[nx][ny] = res[x][y]+1;
+                    q.push({nx,ny});
                 }
             }
         }
+        return res;
 
-        return result;
     }
 };
