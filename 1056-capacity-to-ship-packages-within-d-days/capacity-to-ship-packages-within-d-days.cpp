edb1bcdb0254cee32d707b3f155c9ae47ef854aa
Yash Rohilla <110462053+Yash1753@users.noreply.github.com>
2025-09-04 23:44:26 +0530

    Time: 11 ms (67.72%) | Memory: 35.1 MB (46.77%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
@@ -1,38 +1,37 @@
 class Solution {
 public:
-    bool helper(vector<int>&a,int days ,int mid){
+    bool isValid(vector<int>&a, int days ,int mid){
         int n  = a.size();
         int d = 1,load = 0;
         for(int i =0 ; i<n ; i++){
-            load += a[i];
-            if (load > mid) {
+            if (load + a[i] > mid) {
             d+= 1; //move to next day
             load = a[i]; //load the weight.
         }
-       
+        else {
             //load the weight on the same day.
-            
-        
+            load += a[i];
+        }
         }
 
         
         if(d<= days)return true;
         return false;   
-
     }
     int shipWithinDays(vector<int>& weights, int days) {
-        int high = accumulate(weights.begin(), weights.end(), 0);
-        int low = *max_element(weights.begin(), weights.end());
-        int ans = -1;
-        while(low <= high){
-            int mid = low + (high-low)/2;
-            if(helper(weights,days,mid)){
+        int n = size(weights);
+        int l = *max_element(weights.begin(), weights.end()), r = accumulate(begin(weights),end(weights),0);
+        int ans= -1;
+        while(l<=r){
+            int mid = l + (r-l)/2;
+
+            if(isValid(weights,days,mid)){
                 ans = mid;
-                high = mid-1;
+                r = mid-1;
             }else{
-                low = mid+1;
+                l = mid+1;
             }
         }
         return ans;
     }
-}; 
+};
