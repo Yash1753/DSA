3f8014773447952ccdaa5d21e46040c18ca40719
Yash Rohilla <110462053+Yash1753@users.noreply.github.com>
2025-09-17 13:12:22 +0530

    Time: 31 ms (33.92%) | Memory: 11.2 MB (49.07%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -1,30 +1,30 @@
 class Solution {
 public:
     int countSubstrings(string s) {
-        int n = s.length();
-        int ans = 0;
-        for(int i =0 ; i < n ; i++){
-            int oddExpand = expand(s,i,i);
-            int evenExpand = expand(s,i,i+1);
-            ans+= oddExpand+ evenExpand;
+        //bottom up diret approach h
+        //ek odd evn approach h
+        int n = s.size();
+        vector<vector<bool>>dp(n+1,vector<bool>(n+1));
+
+        for(int i = 0 ; i <= n ;i++){
+            dp[i][i] = true;
         }
-
-        return ans;
-    }
-
-private : 
-    int expand(string s,int i , int j){
         int cnt = 0;
-        while(i >=0 && j< s.length() && s[i] == s[j]){
-            cnt++;
-            i--;
-            j++;
+        //normal template maar do
+        for(int L = 1 ;  L <= n ; L++){
+            for(int i = 0 ; i+L-1 < n ; i++){
+                int j = i + L -1;
+
+                if(i == j) dp[i][j] =  true;
+                else if(i+1 == j) dp[i][j] = s[i] == s[j];
+                else{
+                    dp[i][j] = (s[i] == s[j]) && (dp[i+1][j-1]);
+                }
+
+                if(dp[i][j])cnt++;
+            }
         }
+
         return cnt;
     }
 };
-
-
-
-
-
