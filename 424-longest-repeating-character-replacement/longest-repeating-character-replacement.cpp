dd89e926ce1ecfe1639121395afb1e3a89d44ca0
Yash Rohilla <110462053+Yash1753@users.noreply.github.com>
2025-10-20 00:09:24 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (92.19%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -1,23 +1,27 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        int ans = 0, n = s.size();
-        for(char c = 'A'; c <= 'Z'; c++)
-        {
-            int i = 0, j = 0, replaced = 0;
-            while(j < n)
-            {
-                if(s[j] == c)
-                    j++;
-                else if(replaced < k)
-                    j++, replaced++;
-                else if(s[i] == c)
-                    i++;
-                else
-                    i++, replaced--;
-                ans = max(ans, j - i);
-            }
+        int maxLen = 0;
+        int i = 0 , j = 0;
+        int n = s.length();
+        vector<int>hash(26,0);
+        int maxCnt = 0;
+        while(j < n){
+            hash[s[j]-'A']++;
+            maxCnt = max(maxCnt, hash[s[j]-'A']);
+            //maxLen = max(maxLen, )
+           if((j-i+1) - maxCnt > k){
+                hash[s[i]-'A']--;
+                i++;
+           }
+
+           maxLen = max(maxLen, j-i+1);
+
+
+            j++;
+
         }
-        return ans;
+
+        return maxLen;
     }
 };
