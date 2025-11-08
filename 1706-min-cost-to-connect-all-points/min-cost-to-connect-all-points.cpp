6ca1bcf29652b4ab69f8685a5baf3c60be93087e
Yash Rohilla <110462053+Yash1753@users.noreply.github.com>
2025-11-08 23:34:49 +0530

    Time: 459 ms (37.84%) | Memory: 134.9 MB (35.45%) - LeetSync

--- 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
@@ -1,46 +1,43 @@
 class Solution {
 public:
+    typedef pair<int,int>p;
     int minCostConnectPoints(vector<vector<int>>& points) {
-        //MST WITH prim but graph chiaye toh grap bnata h
-        //pehle adj list bnata h ki from hwere to where 
-        vector<vector<pair<int,int>>> adj(points.size()); // node -> weiht
-        int V = points.size();
-        //created a graph 
-        for(int i = 0 ; i< V; i++){
-            for( int j = i+1 ; j < V ; j++){
-                int dist = abs(points[i][0] - points[j][0]) + abs(points[i][1] - points[j][1]);
+        int n = points.size();
+        //min cost maangi h mst bnao pr uske liye pehle graph bnao
+        unordered_map<int,vector<p>>adj;
+        for(int i = 0 ; i < n  ;i++){
+            for(int j = i+1 ; j < n ; j++){
+                int x1 = points[i][0];int y1 = points[i][1];
+                int x2 = points[j][0];int y2 = points[j][1];
 
+                int dist = abs(x2-x1) + abs(y2-y1);
                 adj[i].push_back({j,dist});
                 adj[j].push_back({i,dist});
             }
         }
 
-        //prim algo minimu spanning tree toh sum maintain krlete h kyuki vhi mujh cost dedega
-        int sum = 0;
-        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<>> pq;
-        pq.push({0,0}); // weight, node maintain krte weight pehle for min
-        vector<bool>visited(V);
-        
+        //graph done
+        //ow we can gop wioth prim
 
+        priority_queue<p,vector<p>,greater<p>>pq;
+        pq.push({0,0});
+        int sum = 0;
+        vector<bool>inMST(n,false);
         while(!pq.empty()){
             auto top = pq.top();pq.pop();
-            int w = top.first;
+            int wt = top.first;
             int node = top.second;
-            if(visited[node]) continue;
 
-            visited[node] = true;
+            if(inMST[node]) continue;
+            inMST[node] = true;
+            sum+=wt;
 
-            sum += w;
-
-            for(auto &tmp : adj[node]){
-               int vw = tmp.second;
-               int v = tmp.first;
-
-                if(!visited[v]){
-                    //visited[v] = true;
-                    pq.push({vw,v});
-                }
-
+            for(auto &v : adj[node]){
+                 int nn = v.first;
+                 int nnW = v.second;
+                 if(!inMST[nn]){
+                    pq.push({nnW,nn});
+                 }
             }
         }
 
