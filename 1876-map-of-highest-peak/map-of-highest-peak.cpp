f6895a24286c3abff1efd2d295b5b926bbae6c47
Yash Rohilla <110462053+Yash1753@users.noreply.github.com>
2025-11-10 00:39:57 +0530

    Time: 79 ms (62.52%) | Memory: 113 MB (53.34%) - LeetSync

--- 1876-map-of-highest-peak/map-of-highest-peak.cpp
+++ 1876-map-of-highest-peak/map-of-highest-peak.cpp
@@ -1,16 +1,21 @@
 class Solution {
 public:
-    typedef pair<int,int>P;
-    vector<vector<int>> directions{{-1,0},{0,-1},{1,0},{0,1}};
     vector<vector<int>> highestPeak(vector<vector<int>>& isWater) {
-        int n = isWater.size();
-        int m = isWater[0].size();
-        queue<P>q;
-        vector<vector<int>> height(n, vector<int>(m,-1));
-        for(int i = 0 ; i < n ; i++){
-            for(int j = 0 ; j < m ; j++){
-                if(isWater[i][j] == 1){
-                    height[i][j] = 0;
+        int m = isWater.size();
+        int n = isWater[0].size();
+
+        //land -> 0
+        //water ->1
+        //height non-neg adn water height = 0;
+        //e cell adjancent diff 1,0
+        vector<pair<int,int>>directions{{1,0},{0,1},{-1,0},{0,-1}};
+        vector<vector<int>>height(m,vector<int>(n,-1));
+        //If the cell is a water cell, its height must be 0. very imp
+        queue<pair<int,int>>q;
+        for(int i = 0 ; i< m ; i++){
+            for(int j = 0 ; j < n ; j++){
+                if(isWater[i][j]){
+                    height[i][j]=0;
                     q.push({i,j});
                 }
             }
@@ -19,28 +24,21 @@ public:
         while(!q.empty()){
             int N = q.size();
             while(N--){
-                auto top = q.front();
-                q.pop();
-                int i = top.first;
-                int j = top.second;
+                auto top = q.front();q.pop();
+                int x =top.first;
+                int y =top.second;
 
                 for(auto &dir : directions){
-                    int new_i = i + dir[0];
-                    int new_j = j + dir[1];
+                    int nx = x + dir.first;
+                    int ny = y + dir.second;
 
-                    if(new_i >=0 && new_i <n && new_j>=0 && new_j < m && height[new_i][new_j] == -1){
-                        q.push({new_i, new_j});
-                        height[new_i][new_j] = height[i][j] +1;
+                    if(nx>=0 && ny>=0 && nx < m && ny < n && height[nx][ny] == -1 ){
+                        height[nx][ny] = height[x][y]+1;
+                        q.push({nx,ny});
                     }
                 }
-
-
             }
         }
-
         return height;
-
-
-
     }
 };
