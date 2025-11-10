d81eb11c1666828e6f18c54fafe7d36f5587258c
Yash Rohilla <110462053+Yash1753@users.noreply.github.com>
2025-11-10 16:40:12 +0530

    Time: 3 ms (95.92%) | Memory: 28.9 MB (96.25%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.cpp
+++ 1073-number-of-enclaves/number-of-enclaves.cpp
@@ -1,49 +1,61 @@
 class Solution {
 public:
-    int m,n;
-    vector<vector<int>> directions{{0,1},{1,0},{-1,0},{0,-1}};
-    bool isValid(int i, int j, vector<vector<int>>& grid){
-        if(i>=0 && i < m && j>=0 && j < n && grid[i][j]==1)return true;
-        return false;
-    }
-    void dfs(int i , int j, vector<vector<int>>& grid){
-        grid[i][j] = 0;
-
-        for(auto &dir : directions){
-            int ni = i + dir[0];
-            int nj = j + dir[1];
-
-            if(isValid(ni,nj,grid)){
-                dfs(ni,nj,grid);
-            }
-        }
-    }
-
-
     int numEnclaves(vector<vector<int>>& grid) {
-        m = grid.size();
-        n = grid[0].size();
+        int m = grid.size();
+        int n = grid[0].size();
 
-        //walk to itgher land cell 1>land 0>sea
-        //boundary traversal ho jaye ga
+        ///0 sea and 1 land
+        vector<pair<int,int>>directions{{1,0},{0,1},{-1,0},{0,-1}};
+        //land cells where we can not
+
+        //1 lelo
+        queue<pair<int,int>>q;
 
         for(int i = 0 ; i < m ; i++){
-            for(int j = 0 ; j < n ; j++){
-                if(i*j == 0 || i == m-1 || j==n-1){
-                    if(grid[i][j] == 1){
-                        dfs(i,j,grid);
+            if(grid[i][0] == 1){
+                grid[i][0] = 0;
+                q.push({i,0});
+            }
+            if(grid[i][n-1] == 1){
+                grid[i][n-1] = 0;
+                q.push({i,n-1});
+            }
+        }
+        for(int j = 0 ; j < n ; j++){
+            if(grid[0][j] == 1){
+                grid[0][j] = 0;
+                q.push({0,j});
+            }
+            if(grid[m-1][j] == 1){
+                grid[m-1][j] = 0;
+                q.push({m-1,j});
+            }
+        }
+
+        while(!q.empty()){
+            int N = q.size();
+            while(N--){
+                auto top = q.front();q.pop();
+                int x = top.first;
+                int y = top.second;
+
+                for(auto &dir:directions){
+                    int nx = x + dir.first;
+                    int ny = y + dir.second;
+
+                    if(nx>=0 && nx < m && ny>=0 && ny<n && grid[nx][ny]==1){
+                        grid[nx][ny] = 0;
+                        q.push({nx,ny});
                     }
                 }
             }
         }
-
-        int ans = 0;
-         for(int i = 0 ; i < m ; i++){
+        int cnt = 0;
+        for(int i = 0 ; i < m ; i++){
             for(int j = 0 ; j < n ; j++){
-               if(grid[i][j] == 1) ans++;
+                if(grid[i][j] == 1) cnt++;
             }
         }
-
-        return ans;
+        return cnt;
     }
 };
