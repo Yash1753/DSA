90d149af1af570e13211d877b64d39a8b506cde0
Yash Rohilla <110462053+Yash1753@users.noreply.github.com>
2025-11-10 15:11:03 +0530

    Time: 8 ms (90.54%) | Memory: 34.9 MB (66.43%) - LeetSync

--- 542-01-matrix/01-matrix.cpp
+++ 542-01-matrix/01-matrix.cpp
@@ -1,41 +1,41 @@
 class Solution {
 public:
-
-
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        //distance and nearest
         int m = mat.size();
         int n = mat[0].size();
-        //multisources bfs
-        vector<vector<int>>directions{{0,1},{1,0},{-1,0},{0,-1}};
         vector<vector<int>>res(m,vector<int>(n,-1));
-        queue<pair<int,int>>q; //coordinates
+        queue<pair<int,int>>q;
+        vector<pair<int,int>>directions{{1,0},{0,1},{-1,0},{0,-1}};
 
-        for(int i = 0 ; i < m ; i++ ){
-            for(int j = 0 ; j  < n ; j++){
+        for(int i = 0 ; i < m ; i++){
+            for(int j = 0 ;j < n ; j++){
                 if(mat[i][j] == 0){
                     res[i][j] = 0;
                     q.push({i,j});
-
                 }
             }
         }
 
         while(!q.empty()){
-            auto front = q.front();q.pop();
-            int x = front.first;
-            int y = front.second;
+            int N = q.size();
+            while(N--){
+                auto top = q.front();q.pop();
+                int x = top.first;
+                int y = top.second;
 
-            for(auto &dir : directions){
-                int nx = x + dir[0];
-                int ny = y + dir[1];
+                for(auto &dir:directions){
+                    int nx = x + dir.first;
+                    int ny = y + dir.second;
 
-                if(nx >=0 && nx < m && ny >=0 && ny < n && res[nx][ny] == -1){
-                    res[nx][ny] = res[x][y]+1;
-                    q.push({nx,ny});
+                    if(nx>=0 && nx < m && ny>=0 && ny < n && res[nx][ny] == -1){
+                        res[nx][ny] = 1 + res[x][y];
+                        q.push({nx,ny});
+                    }
                 }
             }
         }
-        return res;
 
+        return res;
     }
 };
