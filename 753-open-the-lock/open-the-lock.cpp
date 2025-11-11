1bc32311bca1ed379565e38531cefea51df7e5b5
Yash Rohilla <110462053+Yash1753@users.noreply.github.com>
2025-11-11 21:49:52 +0530

    Time: 116 ms (76.42%) | Memory: 38.6 MB (70.40%) - LeetSync

--- 753-open-the-lock/open-the-lock.cpp
+++ 753-open-the-lock/open-the-lock.cpp
@@ -1,48 +1,44 @@
 class Solution {
 public:
     int openLock(vector<string>& deadends, string target) {
-        //ye krte h
-        //can turn one wheel slot
-        //in deadends
+        unordered_set<string>seen(deadends.begin(),deadends.end());
 
         string start = "0000";
         queue<string>q;
         q.push(start);
 
-        unordered_set<string>seen(deadends.begin(),deadends.end());
-
         if(seen.count(start)) return -1;
         seen.insert(start);
-
         int res = 0;
 
         while(!q.empty()){
-            int N =q.size();
+            int N = q.size();
             while(N--){
-                auto word  = q.front();q.pop();
+                string word = q.front();q.pop();
+
                 if(word == target) return res;
 
+                //ab modification
                 for(int i = 0 ; i < 4 ; i++){
+                    //imp yahi ki rotate freely toh upar bhi jaa ksta n nich bhi
                     string inc = word;
                     string dec = word;
-                   
-                        //inc
-                        inc[i] = (inc[i] == '9') ? '0' : inc[i]+1;
-                        if(!seen.count(inc)){
-                            q.push(inc);
-                            seen.insert(inc);
-                        }
-                        dec[i] = (dec[i] == '0') ? '9' : dec[i]-1;
-                        if(!seen.count(dec)){
-                            q.push(dec);
-                            seen.insert(dec);
-                        }
 
-                    
+                    inc[i] = inc[i]=='9'?'0':inc[i]+1;
+                    if(!seen.count(inc)){
+                        q.push(inc);
+                        seen.insert(inc);
+                    }
+                    dec[i] = dec[i]=='0'?'9':dec[i]-1;
+                    if(!seen.count(dec)){
+                        q.push(dec);
+                        seen.insert(dec);
+                    }
                 }
             }
             res++;
         }
+
         return -1;
     }
 };
