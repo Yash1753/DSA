110cd7218f370b16397a3eb70a19fb403e89ce11
Yash Rohilla <110462053+Yash1753@users.noreply.github.com>
2025-11-15 22:46:57 +0530

    Time: 5 ms (36.52%) | Memory: 17.7 MB (84.57%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -1,43 +1,44 @@
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        //n nodes ->
-        unordered_map<int,vector<pair<int,int>>> adj;
-        for(auto &vec :  flights){
-            int u = vec[0];
-            int v = vec[1];
-            int w = vec[2];
+        int ans  =INT_MAX;
+        unordered_map<int,vector<pair<int,int>>>adj;
+        for(auto &edge : flights){
+            int u = edge[0];
+            int v = edge[1];
+            int w = edge[2];
+
             adj[u].push_back({v,w});
         }
-       
         vector<int>dist(n,INT_MAX);
-
+        //disktra
         queue<pair<int,int>>q;
         q.push({src,0});
         dist[src] = 0;
         int steps = 0;
-        while(!q.empty() && steps <=k){
+        while(!q.empty() && steps <= k){
             int N = q.size();
             while(N--){
-                auto top = q.front();q.pop();
-                int u = top.first;  //niode
-                int w = top.second; // wright
+                auto tp = q.front();q.pop();
+                int node = tp.first;
+                int wt = tp.second;
 
-                for(auto &vec : adj[u]){
-                    //pair - > neigh,wt
-                    int v = vec.first;
-                    int wt = vec.second;
+                for(auto &ngr : adj[node]){
+                    int v = ngr.first;
+                    int w = ngr.second;
 
-                    if(w + wt < dist[v]){
+                    if(dist[v] > w + wt){
                         dist[v] = w+wt;
-                        q.push({v,w+wt});
+                        q.push({v,dist[v]});
                     }
+
+
                 }
             }
             steps++;
         }
 
-        if(dist[dst] != INT_MAX) return dist[dst];
-        return -1;
+        return dist[dst] == INT_MAX ? -1 : dist[dst];
+
     }
 };
