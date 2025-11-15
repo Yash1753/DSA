2c517d2d8defe79a0a4a2aee82de56d2803d1f7f
Yash Rohilla <110462053+Yash1753@users.noreply.github.com>
2025-11-15 22:28:57 +0530

    Time: 352 ms (77.99%) | Memory: 355.2 MB (39.26%) - LeetSync

---
+++ 4119-minimum-distance-between-three-equal-elements-ii/minimum-distance-between-three-equal-elements-ii.cpp
@@ -0,0 +1,26 @@
+
+class Solution {
+public:
+    int minimumDistance(vector<int>& nums) {
+        int n = nums.size();
+        if (n < 3) return -1;
+
+        unordered_map<int, vector<int>> mp;
+        mp.reserve(n*2);
+
+        for (int i = 0; i < n; ++i) mp[nums[i]].push_back(i);
+
+        int best = INT_MAX;
+        for (auto &p : mp) {
+            const vector<int>& v = p.second;
+            if (v.size() < 3) continue;
+            // v is already in increasing order of indices
+            for (size_t i = 2; i < v.size(); ++i) {
+                int cur = 2 * (v[i] - v[i-2]);
+                if (cur < best) best = cur;
+            }
+        }
+
+        return (best == INT_MAX) ? -1 : best;
+    }
+};
