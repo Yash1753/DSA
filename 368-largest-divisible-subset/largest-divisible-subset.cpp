5e53a8dae5df0ce9671b54a1857076fbc9b2c4cd
Yash Rohilla <110462053+Yash1753@users.noreply.github.com>
2025-11-20 17:53:15 +0530

    Time: 11 ms (65.62%) | Memory: 12.6 MB (48.25%) - LeetSync

--- 368-largest-divisible-subset/largest-divisible-subset.cpp
+++ 368-largest-divisible-subset/largest-divisible-subset.cpp
@@ -1,70 +1,37 @@
 class Solution {
 public:
-int N;
-   
-
-    void lis(vector<int>& nums, int curr , int prev, vector<int>&result, vector<int>&temp){
-        if(curr >= N){
-            if(temp.size() > result.size()){
-                result = temp;
-                //temp.clear();
-            }
-            return;
-        }
-
-        int take = 0;
-        
-        
-        if(prev == -1 || nums[curr]%nums[prev] == 0 ){
-            temp.push_back(nums[curr]);
-            lis(nums,curr+1,curr,result,temp);
-            temp.pop_back();
-        }
-
-        lis(nums,curr+1,prev,result,temp);
-
-
-        
-
-        
-    }
-
-
+int n;
     vector<int> largestDivisibleSubset(vector<int>& nums) {
-      N = nums.size();
-      if(N == 1) return {nums[0]};
-      sort(nums.begin(),nums.end());
-    vector<int>cnt(N,1);
-    vector<int>ind(N,-1);
-    int maxInd = 0;
-    int maxL = 1;
-    for(int i = 1 ; i < N ; i++){
+     n = nums.size();
+        sort(nums.begin(),nums.end());
+     vector<int>dp(n,1);
+     vector<int>prev(n,-1);
+
+     int lastind = 0;
+     int maxL = 0;
+
+     for(int i = 0; i < n ; i++){
         for(int j = 0 ; j < i ; j++){
             if(nums[i] % nums[j] == 0){
-                if(cnt[i] < cnt[j]+1){
-                    cnt[i] = cnt[j]+1;
-                    ind[i] = j;
+                if(dp[i] < dp[j] + 1){
+                    dp[i] = dp[j]+1;
+                    prev[i] = j;
                 }
 
-                if(cnt[i] > maxL){
-                maxL = cnt[i];
-                maxInd = i;
-
+                if(dp[i] > maxL){
+                    maxL = dp[i];
+                    lastind = i;
+                }
             }
-                
-            }
-
-           
         }
-    }
+     }  
+     vector<int>temp;
+     while(lastind != -1){
+        temp.push_back(nums[lastind]);
+        lastind = prev[lastind];
+     }
 
-    vector<int>result;
+     return temp;
 
-    while(maxInd!= -1){
-        result.push_back(nums[maxInd]);
-        maxInd = ind[maxInd];
-    }
-
-    return result;
     }
 };
