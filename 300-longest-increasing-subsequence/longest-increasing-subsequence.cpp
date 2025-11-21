5a799367f2adec809216f49d889e19214e83d634
Yash Rohilla <110462053+Yash1753@users.noreply.github.com>
2025-11-21 10:27:04 +0530

    Time: 504 ms (22.67%) | Memory: 41.7 MB (34.19%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,19 +1,28 @@
 class Solution {
 public:
-    int helper(int i , int prev , vector<vector<int>>&dp , vector<int>&nums){
-        if(i == nums.size()) return 0;
-
-        if(dp[i][prev+1] != -1 )return dp[i][prev+1];
-        int len = helper(i +1 ,prev , dp,nums);
-        if(prev == -1 || nums[i] > nums[prev]){
-            len = max(len, 1+ helper(i+1, i,dp , nums));
+    int n;
+    int dp[2501][2501];
+    int solve(vector<int>& nums,int i, int p){
+        if(i == n){
+            return 0 ;
         }
-        return dp[i][prev+1] = len ;
-    }
+        if(p!= -1 && dp[i][p] != -1) return dp[i][p];
+        //taken and notTaken
+        int taken = INT_MIN;
+        int notTaken = INT_MIN;
 
+        if(p == -1 || nums[i] > nums[p]){
+            taken = 1 + solve(nums,i+1,i);
+        }
+        notTaken = solve(nums,i+1,p);
+        if(p != -1)dp[i][p] = max(taken,notTaken);
+        return  max(taken,notTaken);
+    }
     int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
-        vector<vector<int>> dp (n , vector<int>(n,-1));
-        return helper(0,-1,dp,nums);
+        //ek prev main krlege to throw in recurisom
+        n = nums.size();
+        memset(dp,-1,sizeof(dp));
+
+        return solve(nums,0,-1);
     }
 };
