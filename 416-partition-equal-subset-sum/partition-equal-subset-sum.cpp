63e373849bf7453f4663ff288e933e133c0192d3
Yash Rohilla <110462053+Yash1753@users.noreply.github.com>
2025-11-21 19:59:55 +0530

    Time: 241 ms (8.05%) | Memory: 167.3 MB (10.74%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,26 +1,47 @@
 class Solution {
 public:
+
     int n;
-    vector<vector<int>>dp;
-    bool helper(int i,int sum, int target , vector<int>&nums){
-        if(i >=n ) return false;
-        if(sum == target) return true;
-        if(dp[i][sum] != -1) return dp[i][sum];
-        //taek not take
-        
-        bool take =  nums[i] + sum <= target ?  helper(i+1,sum+ nums[i], target,nums) : false;
-        bool nottake =  helper(i+1,sum, target,nums);
-        return dp[i][sum]=take || nottake;
+    //ab krege optimize for that use dp as overlap maar rha
+    //2d for sum and idx
+    int totalSum;
+    
+    bool solve(vector<int>&nums, int &sum, int i, int &curr,  vector<vector<int>>&dp){
+        if(i == n){
+            if(curr == sum) return true;
+            else return false;
+        }
+
+        if(dp[i][curr] != -1) return dp[i][curr];
+
+        bool take = false;
+        bool notTake = false;
+
+        //only take when it is less
+        if(curr + nums[i] <= sum){
+            curr += nums[i];
+            take = solve(nums,sum,i+1,curr,dp);
+            curr -= nums[i];
+        }
+
+        notTake = solve(nums,sum,i+1,curr,dp);
+
+        return dp[i][curr]=take || notTake;
     }
     bool canPartition(vector<int>& nums) {
+        //subset bnana h where sum in both is true
+        //toh naturally whole sum of array should be even
         n = nums.size();
-        int sum = 0;
-        for(auto it : nums){
-            sum+=it;
-        }
-        if(sum%2) return false;
-        dp.resize(n,vector<int>(sum/2+1,-1));
+
+        totalSum = accumulate(nums.begin(),nums.end(),0);
+
+        if(totalSum%2) return false;
+        totalSum = totalSum/2;
+        int curr = 0;
+        //int dp[201][totalSum];
+        vector<vector<int>>dp(201,vector<int>(totalSum+1,-1));
+        //memset(dp,-1,sizeof(dp));
+        return solve(nums,totalSum,0,curr,dp);
         
-        return helper(0,0,sum/2, nums);
     }
 };
