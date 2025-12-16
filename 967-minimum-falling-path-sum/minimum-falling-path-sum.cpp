3b3b7e16ba4df13d379dd40fcae231b45a601325
Yash Rohilla <110462053+Yash1753@users.noreply.github.com>
2025-12-16 11:50:05 +0530

    Time: 4 ms (21.23%) | Memory: 14.9 MB (59.88%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -1,46 +1,29 @@
 class Solution {
 public:
-
-   /*  int helper(int i , int j , vector<vector<int>> &dp , vector<vector<int>> & matrix ){
-        if(j < 0 || j>= matrix[0].size()) return 1e9;
-        if(i == 0 ) return matrix[0][j];
-
-
-        if(dp[i][j] != -1) return dp[i][j];
-        int up = matrix[i][j] + helper(i-1 , j , dp , matrix);
-        int left = matrix[i][j] + helper(i-1 , j-1 , dp , matrix);
-        int right = matrix[i][j] + helper(i-1, j+1 , dp , matrix);
-
-        return dp[i][j] = min(up,min(left,right));
-    }     YAHA TLE TOH TABULATION LGAO*/
-
     int minFallingPathSum(vector<vector<int>>& matrix) {
-        vector<vector<int>> dp (matrix.size() , vector<int>(matrix[0].size(), 0));
-        int mini = INT_MAX;
-        
-        int n = matrix.size();
-        int m = matrix[0].size();
+        int m = matrix.size();
+        int n = matrix[0].size();
 
-        for(int j =0 ; j< m ; j++){
-            dp[0][j]= matrix[0][j];
+        vector<vector<int>>dp(m,vector<int>(n,0));
+
+        for(int i = 0 ; i < n ; i++){
+            dp[0][i] = matrix[0][i];
         }
 
-        for(int i = 1 ; i<n; i++){
-            for(int j = 0 ; j < m ; j++){
-                int l = INT_MAX;
-                int r = INT_MAX;
-                int s = matrix[i][j] + dp[i-1][j];
-                if(j-1 >=0)  l = matrix[i][j] + dp[i-1][j-1];
-                if(j+1 < m)  r = matrix[i][j] + dp[i-1][j+1];
+        for(int i = 1 ; i < m ; i++){
+            for(int j = 0 ; j < n ; j++){
+                int down =  dp[i-1][j];
+                int left = INT_MAX;
+                int right = INT_MAX;
+                if(j> 0) left =  dp[i-1][j-1]; 
+                if(j < n-1) right = dp[i-1][j+1];
 
-                dp[i][j] = min(s, min(l,r));
+                dp[i][j] = matrix[i][j] +min({down,left,right}); 
             }
         }
-       
-        for(int j = 0 ; j <m ; j++){
-            mini = min(mini, dp[n-1][j]);
 
-        }
-        return mini;
+        int ans = *min_element(dp[m-1].begin(), dp[m-1].end());
+
+        return ans;
     }
 };
