d35fff5a6f3142097a70b438b0e68e6ad8157eb3
Yash Rohilla <110462053+Yash1753@users.noreply.github.com>
2025-12-21 17:12:52 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (26.48%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.cpp
+++ 410-split-array-largest-sum/split-array-largest-sum.cpp
@@ -1,41 +1,45 @@
 class Solution {
 public:
-    int part(vector<int>&a , int maxSum){
+    int n;
+
+    bool fn(int mid,vector<int>& nums, int k){
         long long sum = 0;
-        int partition = 1;
-        int n = a.size();
-        for(int i= 0 ; i<n;i++){
-            if(sum+a[i] <=maxSum){
-                sum+=a[i];
+
+        int cnt = 1;
+
+        for(int i = 0 ; i < n ; i++){
+            if(sum + nums[i] <= mid){
+                sum += nums[i];
             }else{
-                partition++;
-                sum = a[i];
+                cnt++;
+                sum = nums[i];
             }
         }
 
-        return partition;
+        return cnt <=k;
     }
 
     int splitArray(vector<int>& nums, int k) {
-        int low = *max_element(nums.begin(),nums.end());
-        int high = accumulate(nums.begin(),nums.end(),0);
-        //,linear search tle maarega
-        /* for(int i = low ; i <= high ; i++){
-            if(part(nums,i) == k) return i;
-        } */
+        //largest sum is minimized
+        //k subarrays bnane h
 
-        //usingbinary search
-        int ans = low;
-        while(low <=high){
-            int mid = low +(high-low)/2;
-            
-            if(part(nums,mid) <=k ){
+        n = nums.size();
+        int low = *max_element(nums.begin(), nums.end());
+        int high = accumulate(nums.begin(),nums.end(),0);
+
+        int ans;
+
+        while(low <= high){
+            int mid = low + (high-low)/2;
+
+            if(fn(mid,nums,k)){
                 ans = mid;
                 high = mid-1;
             }else{
                 low = mid+1;
             }
         }
+
         return ans;
     }
 };
