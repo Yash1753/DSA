018364c960381d247539fa4c0042cc5189c1cb86
Yash Rohilla <110462053+Yash1753@users.noreply.github.com>
2025-12-21 11:25:01 +0530

    Time: 21 ms (7.55%) | Memory: 22.9 MB (43.72%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -4,7 +4,7 @@ public:
     bool fn(int mid, vector<int>& piles, int h){
         bool canEat = false;
 
-        int timetoEat = 0;
+        long long timetoEat = 0;
 
         for(int i = 0; i < piles.size();i++){
             timetoEat += ceil((double)piles[i]/(double)mid);
@@ -27,16 +27,16 @@ public:
         int high = *max_element(piles.begin(),piles.end());
         int ans = INT_MAX;
 
-        while(low <high){
+        while(low <=high){
             int mid = low + (high-low)/2;
 
             if(fn(mid,piles,h)){
                 ans = mid;
-                high = mid;
+                high = mid-1;
             }else{
                 low = mid+1;
             }
         }
-        return low;
+        return ans;
     }
 };
