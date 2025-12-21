62c0bc518171198e90c366963e86173bc9500164
Yash Rohilla <110462053+Yash1753@users.noreply.github.com>
2025-12-21 11:47:20 +0530

    Time: 7 ms (90.62%) | Memory: 34.9 MB (84.56%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
@@ -1,37 +1,41 @@
 class Solution {
 public:
-    bool isValid(vector<int>&a, int days ,int mid){
-        int n  = a.size();
-        int d = 1,load = 0;
-        for(int i =0 ; i<n ; i++){
-            if (load + a[i] > mid) {
-            d+= 1; //move to next day
-            load = a[i]; //load the weight.
-        }
-        else {
-            //load the weight on the same day.
-            load += a[i];
-        }
+
+    bool fn(int mid,vector<int>& weights, int days){
+        long long sum = 0;
+        int d = 1;
+
+        for(auto it : weights){
+            if(sum + it > mid){
+                d += 1;
+                sum = 0;
+            }
+            sum += it;
         }
 
-        
-        if(d<= days)return true;
-        return false;   
+        return d <=days;
     }
-    int shipWithinDays(vector<int>& weights, int days) {
-        int n = size(weights);
-        int l = *max_element(weights.begin(), weights.end()), r = accumulate(begin(weights),end(weights),0);
-        int ans= -1;
-        while(l<=r){
-            int mid = l + (r-l)/2;
 
-            if(isValid(weights,days,mid)){
+    int shipWithinDays(vector<int>& weights, int days) {
+        //withdin days m jayegi shipment
+        //can not load more than max capaictu 
+        //least weight capacity btao within days
+        int n = size(weights);
+        int low = *max_element(weights.begin(), weights.end());
+        int high = accumulate(weights.begin(),weights.end(),0);
+
+        int ans = -1;
+        while(low <= high){
+            int mid = low + (high-low)/2;
+
+            if(fn(mid,weights,days)){
                 ans = mid;
-                r = mid-1;
+                high = mid-1;
             }else{
-                l = mid+1;
+                low = mid+1;
             }
         }
+
         return ans;
     }
 };
