4a38754052861c53158d2871eb15980140b7ce3c
Yash Rohilla <110462053+Yash1753@users.noreply.github.com>
2025-12-22 19:21:49 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (53.00%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
@@ -1,12 +1,18 @@
 class Solution {
 public:
     string removeOuterParentheses(string s) {
+        int level = 0;
         string ans = "";
-        int cnt = 0;
-        for(int i = 0 ; i < s.size() ; i++){
-            if(s[i] == ')') cnt--;
-            if(cnt!=0) ans.push_back(s[i]);
-            if(s[i] == '(') cnt++;
+        for(int i = 0 ; i < s.length() ; i++){
+            if(s[i] == ')'){
+                level--;
+            }
+            if(level != 0){
+                ans += s[i];
+            }
+            if(s[i] == '('){
+                level++;
+            }
         }
 
         return ans;
