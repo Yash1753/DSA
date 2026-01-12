30d70e7e010a59b482c3719731bec561670aa2f2
Yash Rohilla <110462053+Yash1753@users.noreply.github.com>
2026-01-12 12:48:36 +0530

    Time: 0 ms (100.00%) | Memory: 15 MB (79.58%) - LeetSync

---
+++ 1222-remove-covered-intervals/remove-covered-intervals.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int removeCoveredIntervals(vector<vector<int>>& intervals) {
+        int n = intervals.size();
+        sort(intervals.begin(),intervals.end(),[&](vector<int>&a, vector<int>&b){
+            if(a[0] == b[0]) return a[1] > b[1];
+            return a[0] < b[0];
+        });
+
+        int cnt = 0;
+        int lastmax = 0;
+        for(int i = 0 ; i < n ; i++){
+            if(intervals[i][1]  > lastmax){
+                cnt++;
+                lastmax = intervals[i][1];
+            }
+        }
+
+        return cnt;
+    }
+};
