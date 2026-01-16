00231f388ca21f29060d0898ea37930e52504bad
Yash Rohilla <110462053+Yash1753@users.noreply.github.com>
2026-01-16 20:28:01 +0530

    Time: 42 ms (61.93%) | Memory: 45.4 MB (69.91%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,23 +1,31 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        unordered_map<int, int> prefixCount;
-        prefixCount[0] = 1;  // base case
-        
-        int prefixSum = 0;
-        int ans = 0;
-        
-        for (int num : nums) {
-            prefixSum += num;
-            
-            // Check if we’ve seen a prefix sum that would make this subarray sum to k
-            if (prefixCount.count(prefixSum - k)) {
-                ans += prefixCount[prefixSum - k];
+        int cnt = 0;
+        int n = nums.size();
+        /* for(int i = 0 ; i < n ; i++){
+            int sum = 0;
+            for(int j = i ; j < n ; j++){
+                sum += nums[j];
+                if(sum == k) cnt++;
             }
-            
-            prefixCount[prefixSum]++;
+        } */
+        unordered_map<int,int>mp;
+        mp[0] = 1;
+        int sum = 0;
+
+        for(int &num : nums){
+            sum += num;
+
+            if(mp.count(sum-k)){
+                cnt += mp[sum-k];
+            }
+
+            mp[sum]++;
         }
-        
-        return ans;
+
+        return cnt;
+
+       
     }
 };
