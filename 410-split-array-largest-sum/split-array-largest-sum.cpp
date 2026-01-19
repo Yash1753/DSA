bbbfceb612ce481dd521bf79af456e76cec51c6e
Yash Rohilla <110462053+Yash1753@users.noreply.github.com>
2026-01-19 11:09:18 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (89.56%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.cpp
+++ 410-split-array-largest-sum/split-array-largest-sum.cpp
@@ -1,45 +1,39 @@
 class Solution {
 public:
-    int n;
 
-    bool fn(int mid,vector<int>& nums, int k){
-        long long sum = 0;
-
+    bool isPossible(vector<int>&nums, int k , int mid){
+        int n = nums.size();
         int cnt = 1;
+        int sum = 0;
 
-        for(int i = 0 ; i < n ; i++){
-            if(sum + nums[i] <= mid){
-                sum += nums[i];
-            }else{
+        for(int i =  0 ; i < n ; i++){
+            if(sum + nums[i] > mid){
                 cnt++;
                 sum = nums[i];
             }
+            else sum = sum + nums[i];
         }
-
-        return cnt <=k;
+        return cnt<=k;
     }
 
     int splitArray(vector<int>& nums, int k) {
-        //largest sum is minimized
-        //k subarrays bnane h
-
-        n = nums.size();
-        int low = *max_element(nums.begin(), nums.end());
+        //subarray h toh sort toh nhi hoga
+        int n = size(nums);
+        //mimizie the maximum(larget sum) -> go left
+        int low =  *max_element(nums.begin(), nums.end());
         int high = accumulate(nums.begin(),nums.end(),0);
+        int ans = 0;
 
-        int ans;
-
-        while(low <= high){
+        while(low <=high){
             int mid = low + (high-low)/2;
 
-            if(fn(mid,nums,k)){
+            if(isPossible(nums,k,mid)){
                 ans = mid;
                 high = mid-1;
             }else{
                 low = mid+1;
             }
         }
-
         return ans;
     }
 };
